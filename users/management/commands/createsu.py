0de6b2ca2513ad13aa0d6dece73f28924fff9102
JHoo0118 <kik3078@gmail.com>
2020-03-12 14:51:25 +0900

    슈퍼유저 #2

--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -4,12 +4,12 @@ from users.models import User
 
 class Command(BaseCommand):
 
-    help = "This command creates superuser"
+    help = "슈퍼 유저 생성"
 
     def handle(self, *args, **options):
         admin = User.objects.get_or_none(username="admin")
         if not admin:
             User.objects.create_superuser("admin", "kik3078@naver.com", "1234")
-            self.stdout.write(self.style.SUCCESS(f"슈퍼유저 생성되었습니다."))
+            self.stdout.write("슈퍼유저가 생성되었습니다.")
         else:
-            self.stdout.write(self.style.SUCCESS(f"슈퍼유저가 존재합니다."))
+            self.stdout.write("슈퍼유저가 존재합니다.")
