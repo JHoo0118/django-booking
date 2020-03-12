f5230ba9a6b0d4a510c2ab1da454ad6bc1f52bb2
JHoo0118 <kik3078@gmail.com>
2020-03-12 14:54:48 +0900

    슈퍼유저 #3

--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -10,6 +10,3 @@ class Command(BaseCommand):
         admin = User.objects.get_or_none(username="admin")
         if not admin:
             User.objects.create_superuser("admin", "kik3078@naver.com", "1234")
-            self.stdout.write("슈퍼유저가 생성되었습니다.")
-        else:
-            self.stdout.write("슈퍼유저가 존재합니다.")
